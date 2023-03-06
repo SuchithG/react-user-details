076fdaad84cc8c1bb397be87720ad3563c126d5e
SuchithG <suchithgowda1995@gmail.com>
2023-03-06 23:06:10 +0530

    feat: :sparkles: Added user input-state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "../UI/Button";
 
 import Card from "../UI/Card";
 import classes from "../Users/AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState();
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
   return (
     <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
       <label htmlFor="username">UserName</label>
-      <input id="username" type="text" />
+      <input id="username" type="text" onChange={usernameChangeHandler} />
       <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="numbers" />
+      <input id="age" type="numbers" onChange={ageChangeHandler}/>
       <Button type="submit">Add User</Button>
     </form>
     </Card>
